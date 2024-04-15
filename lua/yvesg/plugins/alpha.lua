920889080280cb58a6dbaa71cc126442772daddc
yves848 <yves.godart@gmail.com>
2024-04-15 18:43:18 +0200

    test

--- lua/yvesg/plugins/alpha.lua
+++ lua/yvesg/plugins/alpha.lua
@@ -1,9 +1,9 @@
 return {
   "goolord/alpha-nvim",
   event = "VimEnter",
-  config = function ()
-    local alpha=require("alpha")
-    local dashboard=require("alpha.themes.dashboard")
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
     -- Set header
     dashboard.section.header.val = {
       "                                                     ",
@@ -16,12 +16,12 @@ return {
       "                                                     ",
     }
     dashboard.section.buttons.val = {
-      dashboard.button("e", "📄 > New File","<cmd>ene<CR>"),
-      dashboard.button("SPC ee","📁 > Toggle File Explorer","<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff","🔎 > Find File","<cmd>Telescope find_file<CR>"),
-      dashboard.button("SPC fs","🔍 > Find Word","<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr","⏰ > Restore Session For Current Directory","<cmd>SessionRestore<CR>"),
-      dashboard.button("q","❌ > Quit NVIM","<cmd>qa<CR>"),
+      dashboard.button("e", "📄 > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "📁 > Toggle File Explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "🔎 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "🔍 > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "⏰ > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "❌ > Quit NVIM", "<cmd>qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
